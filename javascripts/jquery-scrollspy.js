7c13d0ec648fadc4a3095d71f952d3d25b16d61e
Vagmi Mudumbai <vagmimud@thoughtworks.com>
2013-02-14 00:41:41 +0530

    better formatting and print support

--- javascripts/jquery-scrollspy.js
+++ javascripts/jquery-scrollspy.js
@@ -97,15 +97,3 @@
 
     
 })( jQuery, window );
-$(document).ready(function(){
-  var t=$("#exp-nav").position().top;
-  var b=$("#experience").position().top+$("#experience").height();
-  $("#exp-nav").scrlspy({min: t, max: b,
-                         onEnter: function(e,pos){
-                           $("#exp-nav").addClass("affix");
-                          },
-                         onLeave: function(e,pos){
-                           $("#exp-nav").removeClass("affix");
-                          },
-                        });
-});
